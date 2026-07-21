944bef638340ce2493099f1429b8f17a19afde8d
admurphye <a.d.murphye@gmail.com>
2026-07-21 15:31:57 +0000

    refactor(types): prepare stitch metadata for turning chains

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,17 +1,32 @@
 export type Stitch = {
   symbol: string;
+
   parents: number[];
+
   produces: number;
   consumes: number;
+
   childIndex?: number;
   childCount?: number;
+
+  /**
+   * Nombre de symboles représentés.
+   * Par défaut = 1
+   */
+  count?: number;
+
+  /**
+   * Type particulier de la maille.
+   */
+  kind?: "normal" | "turningChain";
 };
-export type PositionedStitch =
-  Stitch & {
-    x: number;
-    y: number;
-  };
-  export type PositionedCircularStitch = Stitch & {
+
+export type PositionedStitch = Stitch & {
+  x: number;
+  y: number;
+};
+
+export type PositionedCircularStitch = Stitch & {
   x: number;
   y: number;
 
